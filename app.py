e67fe163aa8cb4d76f7ee03e4ac45f224a936fbf
VidyaSinha <vidyasinha939@gmail.com>
2025-04-07 17:17:17 +0530

    login woking in host

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ app.config['SESSION_COOKIE_HTTPONLY'] = True
 
 # ✅ Correct CORS setup
 CORS(app, supports_credentials=True, origins=[
-    "http://localhost:8080"  
+    "http://localhost:8080"  # 👈 must match exactly!
 ])
 
 Session(app)
