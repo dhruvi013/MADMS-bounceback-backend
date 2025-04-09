21140960e65a2869b1fedfa5cbadd3dcb7cfc85e
VidyaSinha <vidyasinha939@gmail.com>
2025-04-09 18:21:31 +0530

    nnnn

--- models/students.py
+++ models/students.py
@@ -6,15 +6,15 @@ from sqlalchemy import Column, Integer, String, DateTime
 
 class Student(Base):
     __tablename__ = "students"
-
+    student_id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     enrollment_number = Column(String)
     student_type = Column(String)
     batch_period = Column(String)
-    gr_no = Column(String)
+    gr_no = Column(Integer)
     pcm = Column(Integer)  # ✅ If not, add this
-    tenth = Column(String)
-    twelfth = Column(String)
+    tenth = Column(Integer)
+    twelfth = Column(Integer)
     acpc = Column(String)
     admission_quota = Column(String)
     nationality = Column(String)
